f361ccff7bd07d825fc27db45acd443947e2e603
PGAlmeida <pedro8tp@gmail.com>
2024-03-18 11:18:44 -0300

    Update do Banco Escola

---
+++ update_escola.sql
@@ -0,0 +1,9 @@
+/* Update nas tabelas*/
+update cursos set nome = 'Ciência de Dados' where id_curso = 4;
+
+/* Update nas tabelas*/
+update estados set nome = 'Minas Gerais' where id_estado = 3;
+
+/* Update nas tabelas*/
+update estados set sigla = 'MG' where id_estado = 3;
+
