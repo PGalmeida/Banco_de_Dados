931dbf007ac0f760bf7fadde42df0b50ecf73425
PGAlmeida <pedro8tp@gmail.com>
2024-03-18 11:20:49 -0300

    Select do Banco Clinica

---
+++ select_clinica.sql
@@ -0,0 +1,15 @@
+/*Insert para tabela Medico*/
+insert into Medico (CRM, Nome, Idade, Especialidade) values ('45896412-3/BR','Adolfo',48,'Pediatria');
+insert into Medico (CRM, Nome, Idade, Especialidade) values ('15796235-8/BR','Luísa',25,'Oftalmologia');
+insert into Medico (CRM, Nome, Idade, Especialidade) values ('75465429-6/US','Joseph',30,'Cardiologia');
+insert into Medico (CRM, Nome, Idade, Especialidade) values ('74125896-3/UK','George',57,'Psiquiatria');
+insert into Medico (CRM, Nome, Idade, Especialidade) values ('50304871-4/BR','Maria Clara',35,'Ginecologia');
+
+/*Insert para tabela Paciente*/
+insert into Paciente (Nome, CPF) values ('','');
+insert into Paciente (Nome, CPF) values ('','');
+insert into Paciente (Nome, CPF) values ('','');
+insert into Paciente (Nome, CPF) values ('','');
+insert into Paciente (Nome, CPF) values ('','');
+insert into Paciente (Nome, CPF) values ('','');
+insert into Paciente (Nome, CPF) values ('','');
